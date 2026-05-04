a6796b85f1bf9bebd46013ed4ddef8ffb3b75285
NetRunner <128007180+net-learner-04@users.noreply.github.com>
2026-05-04 22:20:12 +0900

    Integrate Iptables rule creation and application

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
+mod config;
 mod parser;
 mod pkt_control;
+use config::Iptables;
 use std::io;
 
 fn main() -> io::Result<()> {
+    config::root_check();
+
+    let rule = Iptables::new(
+        Some("filter".to_string()),
+        "-I".to_string(),
+        "OUTPUT".to_string(),
+        vec![
+            "-p".to_string(),
+            "tcp".to_string(),
+            "--dport".to_string(),
+            "443".to_string(),
+        ],
+        "NFQUEUE".to_string(),
+        vec!["--queue-num".to_string(), "0".to_string()],
+    );
+
+    rule.apply()?;
+
     pkt_control::start_control()?;
+
     Ok(())
 }
